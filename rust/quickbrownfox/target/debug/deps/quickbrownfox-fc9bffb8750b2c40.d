39fc0ba3041c64a8d94545716e239ad8aa8d7076
theteamaker <kingwashboard@gmail.com>
2020-01-25 20:03:33 -0330

    quickbrownfox

--- rust/quickbrownfox/target/debug/deps/quickbrownfox-fc9bffb8750b2c40.d
+++
@@ -1,5 +0,0 @@
-/home/evelyn/Documents/kattis/rust/quickbrownfox/target/debug/deps/quickbrownfox-fc9bffb8750b2c40: src/main.rs
-
-/home/evelyn/Documents/kattis/rust/quickbrownfox/target/debug/deps/quickbrownfox-fc9bffb8750b2c40.d: src/main.rs
-
-src/main.rs:
--- rust/quickbrownfox/target/debug/quickbrownfox.d
+++
@@ -1,1 +0,0 @@
-/home/evelyn/Documents/kattis/rust/quickbrownfox/target/debug/quickbrownfox: /home/evelyn/Documents/kattis/rust/quickbrownfox/src/main.rs
