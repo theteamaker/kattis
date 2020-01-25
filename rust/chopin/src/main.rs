12d8f025c169ba1b30289b953939e098b4981881
theteamaker <kingwashboard@gmail.com>
2020-01-25 17:46:16 -0330

    did chopin in rust

---
+++ rust/chopin/src/main.rs
@@ -0,0 +1,33 @@
+use std::io::{self, BufRead};
+use std::collections::HashMap;
+
+fn main() {
+    let stdin = io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().map(|x| x.unwrap()).collect();
+
+    let replacements: HashMap<&str, &str> = [
+        ("Ab minor", "G# minor"), 
+        ("A# minor", "Bb minor"), 
+        ("Db minor", "C# minor"),
+        ("D# minor", "Eb minor"),
+        ("Gb minor", "F# minor"),
+        ("A# major", "Bb major"),
+        ("C# major", "Db major"),
+        ("D# major", "Eb major"),
+        ("Gb major", "F# major"),
+        ("G# major", "Ab major")
+        ].iter().cloned().collect();
+    
+    let mut counter = 1;
+
+    for line in lines {
+        let str_line: &str = &line[..];
+
+        match replacements.get(str_line) {
+            Option::None => println!("Case {}: UNIQUE", counter),
+            Option::Some(i) => println!("Case {}: {}", counter, i),
+        }
+
+        counter = counter + 1;
+    }
+}
