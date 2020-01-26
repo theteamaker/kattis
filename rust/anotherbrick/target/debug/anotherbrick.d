f451ea1e3e5b2c3fd93f56e9134036a69b55ebaa
Evelyn-Nicole May <nathaniel.lidstone@outlook.com>
2020-01-26 01:29:49 -0330

    idk

---
+++ rust/anotherbrick/target/debug/anotherbrick.d
@@ -0,0 +1,1 @@
+/home/evelyn/Documents/kattis/rust/anotherbrick/target/debug/anotherbrick: /home/evelyn/Documents/kattis/rust/anotherbrick/src/main.rs
---
+++ rust/anotherbrick/target/debug/deps/anotherbrick-591ea52490cc769a.d
@@ -0,0 +1,5 @@
+/home/evelyn/Documents/kattis/rust/anotherbrick/target/debug/deps/anotherbrick-591ea52490cc769a: src/main.rs
+
+/home/evelyn/Documents/kattis/rust/anotherbrick/target/debug/deps/anotherbrick-591ea52490cc769a.d: src/main.rs
+
+src/main.rs:
