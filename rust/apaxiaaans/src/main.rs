d6bdbfb8b26d4a913807982c36f93a3c938000d9
theteamaker <kingwashboard@gmail.com>
2020-03-13 19:48:39 -0230

    fixed up some spacing

--- rust/apaxiaaans/src/main.rs
+++ rust/apaxiaaans/src/main.rs
@@ -4,9 +4,7 @@ fn main() {
     let mut line = String::new();
     let stdin = io::stdin();
     stdin.lock().read_line(&mut line).unwrap();
-
     let characters: Vec<char> = line.chars().collect();
-
     let mut new_string = String::new();
 
     for (index, character) in characters.iter().enumerate() {
